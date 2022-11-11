5adb62d91460ff16fc2c0d235e44ae5a2a014eb5
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-11-11 13:53:32 +0100

    feat(article): create article model

--- angular/src/app/admin/article.service.ts
+++ angular/src/app/admin/article.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Article } from '../models/article.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class ArticleService {
 
   constructor(private httpClient: HttpClient) { }
 
-  createArticle = (article: any) => {
-    return this.httpClient.post<any>('http://localhost:3000/articles', article);
+  createArticle = (article: Article) => {
+    return this.httpClient.post<Article>('http://localhost:3000/articles', article);
   }
 }
