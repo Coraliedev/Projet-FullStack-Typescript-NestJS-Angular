3b78f9c9ac6e3cb190888f2128296d0e8a85a0aa
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-11-11 13:14:51 +0100

    feat(admin): create admin module

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
 ];
 
 @NgModule({
