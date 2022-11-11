4a1f93d57ce8155ca6fe0ff58907a0c85afeb7ea
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-11-11 15:03:22 +0100

    feat(article): create interceptor to add author by default to the request body

--- cms-nestJS/src/articles/articles.controller.ts
+++ cms-nestJS/src/articles/articles.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
+import { CheckauthorInterceptor } from 'src/checkauthor/checkauthor.interceptor';
 import { ArticlesService } from './articles.service';
 
 @Controller('articles')
@@ -6,6 +7,7 @@ export class ArticlesController {
   constructor(private readonly articlesServices: ArticlesService) {}
 
   @Post()
+  @UseInterceptors(CheckauthorInterceptor)
   async createArticle(@Body() createArticleDto) {
     return this.articlesServices.create(createArticleDto);
   } 
---
+++ cms-nestJS/src/checkauthor/checkauthor.interceptor.ts
@@ -0,0 +1,16 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CheckauthorInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    console.log('before', request.body);
+    if (!request.body.author) {
+      request.body.author = 'Coralie';
+      console.log('after', request.body);
+    }
+    
+    return next.handle();
+  }
+}
