005171c3544ed1f54d277ea10c1b736e0a4ab495
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-11-11 15:35:23 +0100

    feat(delete): create delete service

--- angular/src/app/admin/article.service.ts
+++ angular/src/app/admin/article.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Article } from '../models/article.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticleService {
+  private readonly API_URL = 'http://localhost:3000/articles';
+  private httpHeaders = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   createArticle = (article: Article) => {
-    return this.httpClient.post<Article>('http://localhost:3000/articles', article);
-  }
+    return this.httpClient.post<Article>(this.API_URL, article);
+  };
+
+  deleteArticle = (article: Article) => {
+    const fullURL = `${this.API_URL}/${article._id}`;
+    return this.httpClient.delete<Article>(fullURL, this.httpHeaders);
+  };
 }
