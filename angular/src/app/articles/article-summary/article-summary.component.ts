58069572968a9be277f461197f6d9a28e5553720
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-12-05 12:20:21 +0100

    feat(delete): delete method

--- angular/src/app/articles/article-summary/article-summary.component.ts
+++ angular/src/app/articles/article-summary/article-summary.component.ts
@@ -1,17 +1,45 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { ArticleService } from 'src/app/admin/article.service';
 import { Article } from 'src/app/models/article.model';
 
 @Component({
   selector: 'app-article-summary',
   templateUrl: './article-summary.component.html',
-  styleUrls: ['./article-summary.component.css']
+  styleUrls: ['./article-summary.component.css'],
 })
+
 export class ArticleSummaryComponent implements OnInit {
+
+  isWaitingForServerResponse = false;
+  error = null;
+
   @Input()
   article!: Article;
-  constructor() { }
+  constructor(private articleService: ArticleService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  delete(article: Article) {
+    this.isWaitingForServerResponse = true;
+    this.articleService
+      .deleteArticle(article)
+      .pipe(catchError(this.handleError))
+      .subscribe(data => {
+        this.isWaitingForServerResponse = false;
+        this.handleSuccess(data);
+      }, err => {
+        this.isWaitingForServerResponse = false;
+        this.handleError(err);
+      });
   }
 
+  handleError(err: any) {
+    this.error = err;
+    return throwError(this.error);
+  }
+
+  handleSuccess(data: any) {
+    console.log('success', data);
+  }
 }
