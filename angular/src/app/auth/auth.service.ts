e13a92abfc846b774f7f64e2acafcc9f174a31be
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-12-14 19:43:07 +0100

    feat(auth): post to create a new user

--- angular/src/app/auth/auth.service.ts
+++ angular/src/app/auth/auth.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private baseURL = 'http://localhost:3000/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  register(credentials: any) {
+    const fullURL = `${this.baseURL}/register`;
+    this.http.post<any>(fullURL, credentials).subscribe((createdUser) => {
+      console.log('createdUser',createdUser);
+    });
+  }
 }
