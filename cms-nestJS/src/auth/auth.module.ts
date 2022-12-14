e749d609095864c1f0245cf98fa2c20a73e21295
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-12-14 16:45:08 +0100

    feat(nestJS): create jwt strategy

--- cms-nestJS/src/auth/auth.module.ts
+++ cms-nestJS/src/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { join } from 'path/posix';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
