9dfbdf00c8d5c8d3942305fc74caa6f42ef6c847
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-12-14 13:48:46 +0100

    feat(nestJS): create users module

--- cms-nestJS/src/app.module.ts
+++ cms-nestJS/src/app.module.ts
@@ -3,10 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
+import { UsersModule } from './users/users.module';
 import config from './config';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }), ArticlesModule],
+  imports: [MongooseModule.forRoot(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }), ArticlesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
