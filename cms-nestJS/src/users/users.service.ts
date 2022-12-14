0dd35b1d35d726ae73b5a379087273f1f16a1b74
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-12-14 14:05:58 +0100

    feat(nestJS): create a user to MongoDB via Mongoose

--- cms-nestJS/src/users/users.service.ts
+++ cms-nestJS/src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { create } from 'domain';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return await createdUser.save();
+  }
+}
