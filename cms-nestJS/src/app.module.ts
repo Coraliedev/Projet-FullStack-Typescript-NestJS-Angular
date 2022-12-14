0361fc04b74cd6687084deb1945f8b413dbd8555
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-12-14 16:03:41 +0100

    feat(nestJS): create auth module

--- cms-nestJS/src/app.module.ts
+++ cms-nestJS/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import config from './config';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }), ArticlesModule, UsersModule],
+  imports: [MongooseModule.forRoot(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }), ArticlesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- cms-nestJS/src/users/users.service.ts
+++ cms-nestJS/src/users/users.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { create } from 'domain';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './interfaces/user.interface';
 
 @Injectable()
 export class UsersService {
-  
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const createdUser = new this.userModel(createUserDto);
     return await createdUser.save();
+  }
+
+  async findOne(email: string): Promise<User | undefined> {
+    return await this.userModel.findOne({ email }).exec();
   }
 }
